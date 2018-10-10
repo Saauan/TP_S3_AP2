0a0e200873f57a416450d1182453991e14c9aa69
Ragyan <37266197+Ragyan@users.noreply.github.com>
2018-10-10 13:12:17 +0200

    Avancement textuel
    Mise en place de la grille

--- TP3_Minesweeper/src/console_main.py
+++ TP3_Minesweeper/src/console_main.py
@@ -25,13 +25,35 @@ def main():
         height = int(sys.argv[2])
         nbombs = int(sys.argv[3])
     else:
-        width = 20
-        height = 10
-        nbombs = 0.2*(width*height)
+        width = 30
+        height = 20
+        nbombs = int(0.12*(width*height))
     game = Minesweeper(width, height, nbombs)
+    
+    inplace(width,height)
+
+
+    Actions = input("Your play x,y,C (C=(R)eveal,(S)et,(U)nset): ").split(',')
+    Actions[0],Actions[1] = int(Actions[0]),int(Actions[1])
+    print(Actions)
+
+def inplace(width, height):
+    """
+    create the textual grid of the game
+    """
+    print(" ",end="")
+    for i in range(width):
+        print(" {:3d}".format(i), end="")
+    print("")
+    
+    for j in range(height):
+        print("{:2s}".format("") + width*"+---" + "+")
+        print("{:2s}".format(str(j)) + width*("|" + "{:^3s}".format("")) + "|")
+    print(" " + width*"+---" + "+")
 
 
 if __name__ == '__main__':
     main()
+
 
 
